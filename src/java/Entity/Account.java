d8849062beae75321423eb70f323efbb3a6af317
ThanhTan00 <tan.lenguyen.cit19@eiu.edu.vn>
2023-10-15 22:53:53 +0700

    Update Account.java

--- src/java/Entity/Account.java
+++ src/java/Entity/Account.java
@@ -23,46 +23,6 @@ public class Account {
         this.isAdmin = isAdmin;
     }
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getUser() {
-        return user;
-    }
-
-    public void setUser(String user) {
-        this.user = user;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public int getIsSeller() {
-        return isSeller;
-    }
-
-    public void setIsSeller(int isSeller) {
-        this.isSeller = isSeller;
-    }
-
-    public int getIsAdmin() {
-        return isAdmin;
-    }
-
-    public void setIsAdmin(int isAdmin) {
-        this.isAdmin = isAdmin;
-    }
-
     @Override
     public String toString() {
         return "Account{" + "id=" + id + ", user=" + user + ", password=" + password + ", isSeller=" + isSeller + ", isAdmin=" + isAdmin + '}';
